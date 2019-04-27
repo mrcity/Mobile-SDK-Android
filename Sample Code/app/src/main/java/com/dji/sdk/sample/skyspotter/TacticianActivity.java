b4710e771b53acc7c141f5bb6d8d8b61efb80323
Stephen Wylie <stev-o@stev-o.us>
2019-04-27 02:49:35 -0500

    The actual code for Tactician Activity (early)

--- Sample Code/app/src/main/java/com/dji/sdk/sample/skyspotter/TacticianActivity.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/skyspotter/TacticianActivity.java
@@ -1,13 +1,48 @@
 package com.dji.sdk.sample.skyspotter;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.brother.ptouch.sdk.Printer;
+import com.brother.ptouch.sdk.PrinterInfo;
+import com.dji.sdk.sample.R;
+
+// From https://github.com/kenglxn/QRGen
+import net.glxn.qrgen.android.QRCode;
 
 public class TacticianActivity extends AppCompatActivity {
+
+    Button printQrCodes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tactician);
+
+        printQrCodes = (Button) findViewById(R.id.btn_print_qr_codes);
+
+        printQrCodes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                printBitmap(generateQrCode());
+            }
+        });
+    }
+
+    private Bitmap generateQrCode() {
+        Bitmap myBitmap = QRCode.from("www.example.org").bitmap();
+        return myBitmap;
+    }
+
+    private void printBitmap(Bitmap b) {
+        Printer myPrinter = new Printer();
+        PrinterInfo ptInfo = myPrinter.getPrinterInfo();
+        myPrinter.setPrinterInfo(ptInfo);
+        myPrinter.startCommunication();
+        myPrinter.printImage(b);
+        myPrinter.endCommunication();
     }
 }
