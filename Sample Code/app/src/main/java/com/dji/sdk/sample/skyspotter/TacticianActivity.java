efdfd0239d3014fe8af649df740492c50a0a5ec2
Stephen Wylie <stev-o@stev-o.us>
2019-04-26 23:55:04 -0500

    Tactician Activity and more library commits

---
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/skyspotter/TacticianActivity.java
@@ -0,0 +1,13 @@
+package com.dji.sdk.sample.skyspotter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TacticianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tactician);
+    }
+}
